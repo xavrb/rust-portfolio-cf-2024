68917eb8d7317b6afe9b67159e654c08968d0c19
Javier Mejia <javier.mejia@protonmail.ch>
2024-05-12 17:06:30 -0600

    fibo: done

--- basics/fibonacci/src/main.rs
+++ basics/fibonacci/src/main.rs
@@ -1,6 +1,30 @@
 // Fibonacci Sequence: A program that generates the Fibonacci sequence
 // up to a certain number of terms specified by the user.
+use std::io; // package to use input/output from the standard lib
+
+fn fibo(x: u64) -> u64{
+    if x == 0{
+        0
+    } else if x == 1{
+        1
+    } else{
+        fibo(x-1) + fibo(x-2)
+    }
+}
+
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("Fibonacci number to calculate: \n >>");
+    let mut inputline = String::new();
+    io::stdin().read_line(&mut inputline).expect("Failed to read line!");
+    let inputline: u64 = inputline.trim().parse().expect("Please enter a number!");
+
+
+    for n in 1..inputline{
+        println!("Fibo number is: {}",fibo(n))
+
+    }
+
+
 }
